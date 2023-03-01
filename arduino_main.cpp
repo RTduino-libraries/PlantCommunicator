c1f2eaec0fa857a25b108897f91a363dae4aade7
Stanley L <we.byte.together@gmail.com>
2023-03-01 00:29:39 -0800

    Create arduino_main.cpp

---
+++ arduino_main.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2023-03-01     Stanley Lwin      first version
+ */
+
+#include <Arduino.h>
+#include <CapacitiveSensor.h>
+
+const int writePin = 4;
+const int readPin = 2;
+const int ledPin = D7;
+
+
+CapacitiveSensor capSensor = CapacitiveSensor(writePin,readPin);
+
+void setup(void)
+{
+    Serial.begin();
+    pinMode(ledPin, OUTPUT);
+    capSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
+}
+
+void loop(void)
+{
+   long sensorValue = capSensor.capacitiveSensor(30);
+
+   Serial.println(sensorValue);
+
+   if (sensorValue > 300 )
+   {
+       digitalWrite(ledPin, HIGH);
+   }
+   else
+   {
+       digitalWrite(ledPin, LOW);
+
+   }
+   delay(30);
+}
+
+
