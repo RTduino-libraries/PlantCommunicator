74e4eb6974f0996977c601e5c6506092051a3c73
Stanley L <we.byte.together@gmail.com>
2023-05-04 13:47:54 -0700

    Update def.cpp

--- _Combine/def.cpp
+++ _Combine/def.cpp
@@ -11,19 +11,31 @@ int fadeAmount = 5;
 void read_htaSensor(void)
 {
     sensors_event_t humidity, temp;
-    volatile float tempL = 0.0, humidityL = 0.0;
+    register volatile float tempVal = 0.0, humidityVal = 0.0;
 
     aht.getEvent(&humidity, &temp);
 
     CHECK_SENSOR(temp.temperature, "Temperature");
+    if(sensor_error == ERR)
+    {
+        Serial.println("Restarting AHT sensor");
+        if (!aht.begin())
+          {
+              Serial.println("Could not restart AHT. Check wiring");
+              sensor_error = ERR;
+              while (1) delay(10);
+          }
+        Serial.println("Restarting AHT sensor successful");
+        sensor_error = OK;
+    }
     CHECK_SENSOR(humidity.relative_humidity, "Humidity");
 
-    tempL = TO_FAHRENHEIT(temp.temperature);
-    humidityL = humidity.temperature;
-    Serial.print("Temperature: "); Serial.print(tempL); Serial.println(" degrees F");
-    Serial.print("Humidity: "); Serial.print(humidityL); Serial.println("% rH");
+    tempVal = TO_FAHRENHEIT(temp.temperature);
+    humidityVal = humidity.relative_humidity;
+   Serial.print("Temperature: "); Serial.print(tempVal); Serial.println(" degrees F");
+   Serial.print("Humidity: "); Serial.print(humidityVal); Serial.println("% rH");
 
-    display_oled(&tempL, &humidityL);
+    display_oled(&tempVal, &humidityVal);
 }
 
 void display_oled(volatile float *temp, volatile float *humidity)
@@ -49,7 +61,9 @@ void display_oled(volatile float *temp, volatile float *humidity)
 void read_touchSensor(void)
 {
     long sensorValue = capSensor.capacitiveSensor(30);
-    int ledVal;
+    register volatile int ledVal = 0;
+
+    CHECK_SENSOR(sensorValue, "Touch");
 
     ledVal = map(sensorValue, 0, 2500, 0, 255);
     Serial.print("Captive: "); Serial.println(sensorValue);
@@ -57,7 +71,7 @@ void read_touchSensor(void)
     display_led(&ledVal);
 }
 
-void display_led(int *ledVal)
+void display_led(volatile int *ledVal)
 {
     analogWrite(ledB, 5 - *ledVal); //this is digital using PWM
     analogWrite(ledR, 10 - *ledVal);
@@ -71,20 +85,22 @@ void display_led(int *ledVal)
     }
 }
 
-/*this inline assembly function needs a bug fix*/
-inline float to_fahrenheit(float celsius) {
-    float fahrenheit = 0.00;
-    asm volatile (
-        "push {lr}\n\t"   
-        "lsls %0, %0, #3\n\t"
-        "adds %0, %0, %1\n\t"
-        "adds %0, %0, #32\n\t"
-        "mov %2, %0\n\t"
-        "pop {pc}"
-        : "=r" (fahrenheit)
-        : "r" (celsius), "r" (fahrenheit)
-        : "memory"
-    );
+//inline float to_fahrenheit(float celsius) {
+//    float fahrenheit = 0.00;
+//    asm volatile (
+//        "push {lr}\n\t"     // Save the link register on the stack
+//        "lsls %0, %0, #3\n\t"   // Multiply Celsius by 9 (shift left by 3 is equivalent to multiplication by 8)
+//        "adds %0, %0, %1\n\t"   // Add the result to itself (multiply by 1)
+//        "adds %0, %0, #32\n\t"    // Add 32 to the result to convert to Fahrenheit
+//        "mov %2, %0\n\t"  // Move the result to the fahrenheit variable
+//        "pop {pc}"          // Restore the link register and return
+//        : "=r" (fahrenheit)
+//        : "r" (celsius), "r" (fahrenheit)
+//        : "memory"
+//    );
+//
+//    rt_kprintf("inline asm %f \n", fahrenheit);
+//    return fahrenheit;
+//}
 
-    return fahrenheit;
-}
+
