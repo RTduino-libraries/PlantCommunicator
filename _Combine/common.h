fa65f1b751facc84152d46f8284422a58b38b7a5
Stanley L <we.byte.together@gmail.com>
2023-05-04 13:49:29 -0700

    Update common.h

--- _Combine/common.h
+++ _Combine/common.h
@@ -9,7 +9,6 @@
  * Next step to refactor the code by Mecto to professional code. ^@^
  */
 
-/*std header files, this header files cannot export to C++. That's why declear here*/
 #include <Adafruit_AHTX0.h>
 #include <Arduino.h>
 #include <U8g2lib.h>
@@ -23,21 +22,24 @@
 extern "C"{
 #endif
 
-/* This MACRO is optimized for STM32F401 which provides single-precision FPU.*/
+#define ERR -1
+#define OK 1
+
 #define TO_FAHRENHEIT(celsuis) ((celsuis * 1.8f) + 32.0f)
 
 #define CHECK_SENSOR(data, sensor_name) do { \
     if(!data) { \
         puts(sensor_name "sensor data not available"); \
+        sensor_error = ERR; \
     } \
+    sensor_error = OK; \
 } while(0)
 
 void read_htaSensor(void);
 void display_oled(volatile float *, volatile float *);
 void read_touchSensor(void);
-void display_led(int *);
+void display_led(volatile int *);
 float to_fahrenheit(float celsius);
-
 // Callback methods prototypes
 void htaSensor_Callback();
 void touchSensor_Callback();
@@ -49,8 +51,10 @@ typedef struct ops *ops_t;
 
 extern Adafruit_AHTX0 aht;
 extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
-extern CapacitiveSensor capSensor; 
+extern CapacitiveSensor capSensor; //CapacitiveSensor capSensor = CapacitiveSensor(writePin,readPin);
 extern ops_t local;
+
+extern volatile int sensor_error;
 
 #ifdef __cplusplus
 }
