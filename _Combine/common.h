34c2b7f51c2d6905063d07e1657767eed251894e
Stanley Lwin <98496195+byte-me-stan@users.noreply.github.com>
2023-05-25 19:16:06 -0700

    Update common.h

--- _Combine/common.h
+++ _Combine/common.h
@@ -6,7 +6,7 @@
  * Change Logs:
  * Date           Author            Notes
  * 2023-04-24     Stanley Lwin      first version
- * Next step to refactor the code by Mecto to professional code. ^@^
+ * 
  */
 
 #include <Adafruit_AHTX0.h>
@@ -58,9 +58,6 @@ struct ops{
 };
 typedef struct ops *ops_t;
 
-extern Adafruit_AHTX0 aht;
-extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
-extern CapacitiveSensor capSensor;
 extern ops_t local;
 extern int sensor_error;
 
